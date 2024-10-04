cbd2247dcbdfcc5c7a858e9d07432b389bd80080
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-10-05 00:34:57 +0200

    Journal : created journal entries models

---
+++ app/journal/models.py
@@ -0,0 +1,48 @@
+from pydantic import BaseModel
+from typing import Optional, List
+from enum import Enum
+from sqlalchemy import Column, Integer, String, Boolean, Date,ForeignKey, Enum as SqlEnum
+from sqlalchemy.dialects.postgresql import UUID
+from  sqlalchemy.orm import relationship
+from database  import Base
+import uuid
+from datetime import date
+
+
+
+class MoodRating(str, Enum):
+    awful = 1
+    bad = 2
+    ok = 3
+    good = 4
+    great = 5
+
+class JournalEntryBase(BaseModel):
+    most_important_task: str
+    grateful_things: List[str]
+    overall_day_rating: MoodRating
+    completed_most_important_task: bool
+    day_summary: str
+    mood_tags: Optional[List[str]]
+
+class JournalEntry(JournalEntryBase):
+    id: int
+    user_id: int
+    entry_date: str
+
+    class Config:
+        orm_mode = True
+
+class JournalEntryDB(Base):
+    __tablename__ = "journal_entries"
+
+    id = Column(UUID(as_uuid=True), primary_key=True,default=uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    entry_date = Column(Date, nullable=False,default=date.today)
+    most_important_task = Column(String, nullable=False)
+    grateful_things = Column(String, nullable=False)
+    overall_day_rating = Column(SqlEnum(MoodRating), nullable=False)
+    completed_most_important_task = Column(Boolean, nullable=False)
+    day_summary = Column(String, nullable=False)
+    mood_tags = Column(String, nullable=True)
+    creator = relationship("User",backref="journal_entries")
