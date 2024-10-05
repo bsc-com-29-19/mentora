65e97cbd80220d4cc0829dbdcca59b3e94f754ed
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-10-05 15:08:28 +0200

    project setup : added dependencies

---
+++ dependencies.py
@@ -0,0 +1,26 @@
+from fastapi import Depends, HTTPException
+from database import SessionLocal
+from app.auth.bearer import JWTBearer
+from app.auth.models import User
+from config import get_settings
+import jwt
+
+
+settings = get_settings()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def get_current_user(token:str = Depends(JWTBearer())) -> User:
+    try:
+        payload = jwt.decode(token, f'{settings.SECRET_KEY}', algorithms=['HS256'])
+        user_id = payload.get('sub')
+        db = SessionLocal()
+        return db.query(User).filter(User.id == user_id).first()
+    except(jwt.PyJWTError, AttributeError):
+        return  HTTPException(status_code="Invalid token")
